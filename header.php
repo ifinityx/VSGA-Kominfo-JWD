7b5783bfa2cae3b06218f3e05fe6a1af8a13b2e0
DTM Arifin Ikhwan <85461872+ifinityx@users.noreply.github.com>
2024-07-24 09:20:07 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I-onTopTech Corp.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+
+<body id="home">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
+  <div class="logo container-fluid">
+    <a class="navbar-brand" href="#home">
+      <img src="assets/logo.png" alt="Bootstrap" width="40" height="40">
+      I-onTopTech
+    </a>
+    <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse fw-bold" id="navbarNav">
+      <ul class="navbar-nav flex-grow-1 justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#profile">Company Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#visi">Visi & Misi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#events">Events</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#articles">Articles</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#product">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#galery">Galery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#clients">Clients</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- ENDLIST  -->
+
+<!-- <div class="header text-center"><h1>IonTopTech Corp.</h1></div> -->
+
+<div class="header text-center">
+            <div class="col-md-4"><p><h1>I-onTopTech Corp.</h1></p></div>
+            <!-- <div class="col-md-4"><p><h1>Misi</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem blanditiis pariatur, et ea fugiat quasi id dignissimos at mollitia!</p></div> -->
+    </div>
---
+++ index.php
@@ -0,0 +1,287 @@
+<?php
+    include 'header.php';
+?>
+<!-- Profile -->
+<section id="profile" class="profile container-fluid text-center" >
+    <div class="contrainer">
+        <div class="row text-center ">
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+            <div class="col-md-8"><p><h1>I-onTopTech Corp.</h1>
+            I-onTopTech Corp is an Internet-of-Things company in Indonesia that provides enterprise-grade IoT middleware and devices. With 20 years of experience in embedded and enterprise IT, I-onTopTech Corp is ready to accompany you in entering the industry 4.0.</p></div>
+            <!-- I-onTopTech Corp adalah perusahaan Internet-of-Things di Indonesia yang menyediakan enterprise-grade IoT middleware dan devices. Dengan pengalaman 20 tahun di bidang embedded dan enterprise IT, I-onTopTech Corp siap menemani Anda memasuki industri 4.0.</p></div> -->
+        </div>
+    </div>
+</section>
+<!-- Akhir About -->
+
+<!-- VisiMisi -->
+<section id="visi" class="visi container-fluid text-center" >
+    <div class="contrainer">
+        <div class="row text-center mb-3">
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+            <div class="col-md-4"><p><h1>Visi</h1>Becoming the Main Driver to Enter Industry 4.0 Through the Field of IoT</p></div>
+            <div class="col-md-4"><p><h1>Misi</h1>- Facilitating the Formation of Competent Human Resources in the Field of IoT<br>
+        - Building Standardization and Certification System for IoT<br>- Facilitating Bilateral or Global Collaboration in the Field of IoT</p></div>
+        </div>
+    </div>
+</section>
+<!-- Akhir VisiMisi -->
+
+
+
+<section id="events" class="event container-fluid text-center bg-light text-dark"> <h1>EVENT</h1>
+<!-- <section class="deck m-3 justify-content-center"> -->
+<div class="card-deck">
+  <!-- Card 1 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/events/event1.jpg" class="card-img-top" alt="...">
+    <!-- <div class="card-body"> -->
+    <h5 class="card-text">Consumer Electronics Show 2024</h5>
+    <!-- </div> -->
+  </div>
+  <!-- Card 2 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/events/event2.jpg" class="card-img-top" alt="...">
+    <div class="">
+    <h5 class="card-text text-center">IoT Evolution Expo</h5>
+    </div>
+  </div>
+  <!-- Card 3 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/events/event3.jpg" class="card-img-top" alt="...">
+    <div class="">
+    <h5 class="card-text">SmartHome - Smart Technology </h5>
+    </div>
+  </div>
+
+    <!-- Card 4 -->
+    <div class="card" style="width: 18rem;">
+    <img src="assets/events/event4.jpg" class="card-img-top" alt="...">
+    <div class="">
+    <h5 class="card-text ">Democratization of AI</h5>
+    </div>
+  </div>
+</div>
+</section>
+
+<!-- ARTIKEL -->
+<section id="articles" class="articles container-fluid text-center "> <h1>Articles</h1>
+    <!-- <section class="deck m-3 justify-content-center"> -->
+    <div class="card-deck mt-10">
+    <!-- Card 1 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/articles/articles1.jpg" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title">The Emerging Internet of Things</h5>
+                <p class="card-text text-start">
+                One of the most dynamic and exciting developments in information and communications technology is the advent of the Internet of Things (IoT). Read <a href="#">more.</a></p>
+            </div>
+        </div>
+            <!-- Card 2 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/articles/articles2.jpg" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title">How To Build A Cellular Asset Tracker</h5>
+                <p class="card-text text-start">
+                Asset tracking is a popular use case for IoT. And this is a simple cellular asset tracking solution that provides a great base to customize to your needs. Read <a href="#">more.</a></p>
+            </div>
+        </div>
+            <!-- Card 3 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/articles/articles3.jpg" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title">IoT Platform Build Vs Buy Decision?</h5>
+                <p class="card-text text-start">
+                Build or buy? The purpose of this guide is to help you assess which ones you should build or buy when setting out to build a connected product. Read <a href="#">more.</a></p>
+            </div>
+        </div>
+                <!-- Card 4 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/articles/event1.jpg" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title">The Emerging Internet of Things</h5>
+                <p class="card-text text-start">
+                One of the most dynamic and exciting developments in information and communications technology is the advent of the Internet of Things (IoT)</p>
+            </div>
+        </div>
+    </div>
+</section>
+ <!-- END ARTIKEL  -->
+
+
+<!-- PRODUCT  -->
+ <section id="product" class="product container-fluid text-center bg-light text-dark"> <h1>Product</h1>
+<!-- <section class="deck m-3 justify-content-center"> -->
+<div class="card-deck">
+  <!-- Card 1 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/product/product1.jpg" class="card-img-top" style="width: 100%; height: 298px; object-fit: cover;">
+    <!-- <div class="card-body"> -->
+    <!-- <p class="card-text">1 Januari 1990</p>  -->
+    <h5 class="card-text">Smart Cleaner</h5>
+    <!-- </div> -->
+  </div>
+  <!-- Card 2 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/product/product2.jpg" class="card-img-top" style="width: 100%; height: 298px; object-fit: cover;">
+    <div class="">
+    <!-- <p class="card-text">1 Januari 1990</p>  -->
+    <h5 class="card-text text-center">Virtual Reality</h5>
+    </div>
+  </div>
+  <!-- Card 3 -->
+  <div class="card" style="width: 18rem;">
+    <img src="assets/product/product3.jpg" class="card-img-top" style="width: 100%; height: 298px; object-fit: cover;">
+    <div class="">
+    <!-- <p class="card-text">1 Januari 1990</p>  -->
+    <h5 class="card-text">Smart Feeding</h5>
+    </div>
+  </div>
+
+    <!-- Card 4 -->
+    <div class="card" style="width: 18rem;">
+    <img src="assets/product/product4.jpg" class="card-img-top" style="width: 100%; height: 298px; object-fit: cover;">
+    <div class="">
+    <!-- <p class="card-text">1 Januari 1990</p>  -->
+    <h5 class="card-text ">Augmented Reality</h5>
+    </div>
+  </div>
+</div>
+</section>
+<!-- PRODUCT  -->
+
+<!-- GALERY  -->
+<section id="galery" class="galery container-fluid text-center "> <h1>Galery</h1>
+    <!-- <section class="deck m-3 justify-content-center"> -->
+    <div class="card-deck">
+    <!-- Card 1 -->
+    <div class="card" style="width: 18rem;">
+        <img src="assets/galery/galery1.jpg" class="card-img-top"  style="width: 100%; height: 298px; object-fit: cover;">
+        <!-- <div class="card-body"> -->
+        <!-- <p class="card-text">1 Januari 1990</p>  -->
+        <h5 class="card-text">Consumer Electronics Show 2024</h5>
+        <!-- </div> -->
+    </div>
+    <!-- Card 2 -->
+    <div class="card" style="width: 18rem;">
+        <img src="assets/galery/galery2.jpg" class="card-img-top"  style="width: 100%; height: 298px; object-fit: cover;">
+        <div class="">
+        <!-- <p class="card-text">1 Januari 1990</p>  -->
+        <h5 class="card-text text-center">IoT Evolution Expo</h5>
+        </div>
+    </div>
+    <!-- Card 3 -->
+    <div class="card" style="width: 18rem;">
+        <img src="assets/galery/galery3.jpg" class="card-img-top" style="width: 100%; height: 298px; object-fit: cover;">
+        <div class="">
+        <!-- <p class="card-text">1 Januari 1990</p>  -->
+        <h5 class="card-text">SmartHome - Smart Technology </h5>
+        </div>
+    </div>
+    </div>
+</section>
+<!-- GALERY  -->
+
+<!-- CLIENTS  -->
+<section id="clients" class="clients container-fluid text-center bg-light text-dark"> <h1>Our Clients</h1>
+    <div class="card-deck">
+        <!-- Card 1 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/clients/client1.png" class="card-img-top"  >
+        </div>
+        <!-- Card 2 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/clients/client2.png" class="card-img-top">
+        </div>
+        <!-- Card 3 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/clients/client3.png" class="card-img-top">
+
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="assets/clients/client4.png" class="card-img-top"  >
+        </div>
+        <!-- Card 2 -->
+        <div class="card" style="width: 18rem;">
+            <img src="assets/clients/client5.png" class="card-img-top">
+        </div>
+    </div>
+</section>
+<!-- CLIENTS  -->
+
+
+
+
+<!-- About -->
+<section id="about" class="about container-fluid text-center" >
+    <div class="contrainer">
+        <div class="row text-center ">
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-12"><p><h1>About Us</h1></p></div>
+    
+    </div>
+        <div class="row justify-content-center fs-5 text-center">
+            <img src="assets/profile.jpg" class="card-img-top"  style="width: 15%; height: 250px; object-fit: cover; border-radius:25px;">
+            <div class="col-md-8" style="margin-top: auto; margin-bottom: auto;"><p>Jhon Doe adalah pendiri I-onTopTech Corp. Berawal dari kesenangan pribadi dalam bidang Internet of Things 
+                hingga akhirnya berhasil mendirikan salah satu perusahaan besar dibidang Teknologi.</p></div>
+        </div>
+    </div>
+</section>
+<!-- Akhir About -->
+
+<section id="contact" class="contact container-fluid text-center bg-light text-dark" >
+    <div class="contrainer">
+        <div class="row text-center ">
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-12"><p><h1>Contact Us</h1></p></div>
+    </div>
+        <div class="row justify-content-center fs-5 text-start">
+            <!-- <img src="profile.jpg" class="card-img-top"  style="width: 15%; height: 250px; object-fit: cover; border-radius:25px;"> -->
+                         <!-- ICON  -->
+
+            <div class="col-md-6" style="margin-top: auto; margin-bottom: auto;">
+                <form action="">
+                    <div class="mb-3 row">
+                        <label for="name" class="col-sm-4 col-form-label">Nama</label>
+                        <div class="col-sm-6">
+                        <input type="text" class="form-control" id="name" name="name" readonly>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-sm-4 col-form-label">E-mail</label>
+                        <div class="col-sm-6">
+                        <input type="email" class="form-control" id="email" name="email" readonly>
+
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="name" class="col-sm-4 col-form-label">Your Message</label>
+                        <div class="col-sm-6">
+                        <input type="text" class="form-control" id="name" name="name" readonly>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="name" class="col-sm-4 col-form-label"><button class="btn btn-primary row-sm-4">Pesan</button></label>
+                        <div class="col-sm-6">
+                            <a href="https://instagram.com/ifinity_x" class="icon-link"><i class="fab fa-instagram"></i></a>
+                            <a href="https://facebook.com/arifineinstein07" class="icon-link"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://github.com/ifinityx" class="icon-link"><i class="fab fa-github"></i></a>
+                            <a href="https://www.twitter.com/Ifinity_x" class="icon-link"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.linkedin.com/in/ifinityx77/" class="icon-link"><i class="fab fa-linkedin-in"></i></a> 
+                            <a href="https://www.tiktok.com/@ifinity_x" class="icon-link"><i class="fa-brands fa-tiktok"></i></a> 
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+       
+
+</body>
+</html>
